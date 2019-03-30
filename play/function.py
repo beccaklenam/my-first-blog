971b88da667d3815b283daeb99a4bc63ea94295e
Rebecca Effah Asare <beccaeffah@gmail.com>
2019-03-30 14:01:47 +0000

    my django girl app first commit

---
+++ play/function.py
@@ -0,0 +1,13 @@
+def hi(name):
+	if name == "mary":
+		print("hi mary")
+	elif name == "becca":
+		print("hello becca")
+	elif name == "cynthia":
+		print("hola cynthia")
+	else:
+		print("this is your coach")
+
+coders=["becca","mary","cynthia","gerald"]
+for coder in coders:
+	hi(coder)
---
+++ play/hello.py
@@ -0,0 +1,15 @@
+volume =150
+if volume<20:
+	print("it is kind of quite")
+elif 20<= volume <40:
+	print("it is nice for background music")
+elif 40<= volume <60:
+	print("perfect i can hear all the details")
+elif 60<= volume <80:
+	print("nice for parties")
+elif 80<= volume <100:
+	print("a bit loud")
+else:
+	print("my ears are hurting")
+
+#this is my own code
